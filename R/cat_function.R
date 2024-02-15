0acde62ac2b217775e3f35528ba9792f72796b20
Renay Singh <renaysingh@gmail.com>
2024-02-15 16:49:28 +0200

    Initial commit

---
+++ R/cat_function.R
@@ -0,0 +1,19 @@
+#' A Cat Function
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' cat_function()
+#' @docType package
+#' @name cats
+
+cat_function <- function(love=TRUE){
+  if(love==TRUE){
+    print("I love cats!")
+  }
+  else {
+    print("I am not a cool person.")
+  }
+}
